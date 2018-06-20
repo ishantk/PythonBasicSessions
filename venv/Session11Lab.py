9a9eac8165bb07001c5e9aa5a9fb2153b9dd4c5a
Ishant Kumar <er.ishant@gmail.com>
2018-06-20 14:46:24 +0530

    Session11 - Database Connectivity with python connector

---
+++ venv/Session11Lab.py
@@ -0,0 +1,73 @@
+import mysql.connector
+
+class Student:
+
+    def __init__(self, roll, name, age, email, address):
+        self.roll = roll
+        self.name = name
+        self.age = age
+        self.email = email
+        self.address = address
+
+    def showStudent(self):
+        print("Roll Number {} belongs to {}".format(self.roll,self.name))
+
+    def getStudentDetails(self):
+        data = "{},{},{},{},{}\n".format(self.roll, self.name, self.age, self.email, self.address)
+        return data
+
+s1 = Student(1,"John",20,"john@example.com","Redwood Shores")
+s2 = Student(2,"Jennie",30,"jennie@example.com","Country Homes")
+
+s1.showStudent()
+s2.showStudent()
+
+# Since we wish to persist data
+# Write the data in Database
+
+# Since we wish to persist data
+# Write the data in file
+"""
+file = open("students.txt",'a') #a -> append mode
+data = s2.getStudentDetails()
+file.write(data)
+file.close()
+
+print("Data Saved")
+"""
+
+con = mysql.connector.connect(user="root",password="",host="127.0.0.1",database="GW2018AI")
+print("Is Connection Established:",con.is_connected())
+#print(type(con))
+cursor = con.cursor()
+#print(type(cursor))
+
+# sql1 = "insert into Student values(null,'Fionna',30,'fionna@example.com','Redwood Shores')"
+# sql2 = "insert into Student values(null,'{}',{},'{}','{}')".format(s1.name,s1.age,s1.email,s1.address)
+# sql3 = "insert into Student values(null,'{}',{},'{}','{}')".format(s2.name,s2.age,s2.email,s2.address)
+#
+# cursor.execute(sql2)
+# cursor.execute(sql3)
+# con.commit()
+# print("Student Saved !!")
+
+sql = "select * from Student"
+cursor.execute(sql)
+
+# row = cursor.fetchone()
+# print(row)
+# row = cursor.fetchone()
+# print(row)
+
+students = []
+
+for row in cursor:
+    print(row)
+    s = Student(row[0],row[1],row[2],row[3],row[4])
+    students.append(s)
+
+print(students)
+
+# Read row by row and convert every row into Student Object
+# And create a list of Student objects
+# Sort on the basis of name/roll/age
